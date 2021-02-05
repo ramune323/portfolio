ef15c5d53ed1ce75f03e09f9f86e3da4557b7b39
ramune323 <72336432+ramune323@users.noreply.github.com>
2021-02-05 22:54:17 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,80 @@
+$(function() {
+  // drawer
+  $('.drawer').drawer();
+
+  // textillate
+  $('.js-top-title').textillate();
+  $('.js-top-subtitle').textillate( {
+    initialDelay: 550
+  });
+
+  // wow
+  new WOW().init();
+
+  // smooth scroll
+  jQuery('a[href^="#"]').click(function() {
+    let speed = 300;
+    let id = jQuery(this).attr("href");
+    let target = jQuery("#" == id ? "html" : id);
+    let position = jQuery(target).offset().top;
+    jQuery("html, body").animate(
+      {
+        scrollTop: position
+      },
+      speed
+    );
+    return false;
+  });
+
+  // page_top
+  $(function() {
+    var pagetop = $('#page_top');
+    pagetop.hide();
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        pagetop.fadeIn(300);
+      }
+      else {
+        pagetop.fadeOut(300);
+      }
+    });
+    pagetop.click(function() {
+      $('body, html').animate({ scrollTop: 0 }, 500);
+      return false;
+    });
+  });
+
+  // elevator menu
+  $(window).on('load resize', function() {
+    var stepMenu = function() {
+      var array = {
+        '#top': 0,
+        '#about': 0,
+        '#skills': 0,
+        '#works': 0,
+        '#contact': 0
+      };
+     
+      var $globalNav = new Array();
+     
+      for (var key in array) {
+        if ($(key).offset()) {
+          array[key] = $(key).offset().top - 10;
+          $globalNav[key] = $('.header-nav-item a[href="' +key+ '"]');
+        }
+      }
+     
+      $(window).on('load resize scroll', function () {
+        for (var key in array) {
+          if ($(window).scrollTop() > array[key] - 10) {
+            $('.header-nav-item a').each(function() {
+              $(this).removeClass('current');
+            });
+          $globalNav[key].addClass('current');
+          }
+        }
+      });
+    }
+    stepMenu();
+  });
+});
