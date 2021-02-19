f388abd90b0e6a0c8efb51881e2e208804c827c0
ramune323 <72336432+ramune323@users.noreply.github.com>
2021-02-19 21:58:39 +0900

    add animation

--- js/script.js
+++ js/script.js
@@ -85,4 +85,15 @@ $(function() {
     forceRender: false,
     animTimingFunction: Vivus.EASE
   })
+
+  // animation
+  $(document).ready(function(){
+    $('.animation_wrap').on('animationend', function () {
+        if ($(this).attr('data-order') === 'left') {
+            $(this).attr('data-order', 'right');
+        } else {
+            $(this).attr('data-order', 'left');
+        }
+    });
+  });
 });
